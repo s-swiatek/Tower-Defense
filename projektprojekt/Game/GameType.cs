c80b9adf7725b944cb8520a644cfbf20362a6c8e
Jakub Chrzanowski <kubek1290pl@gmail.com>
2026-02-14 14:56:30 +0100

    Enemy class

---
+++ projektprojekt/Game/GameType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace projektprojekt.Game;
+
+public sealed class Enemy
+{
+    public required EnemyType Type { get; init; }
+    public float Hp { get; set; }
+    public float MaxHp { get; init; }
+
+    public float Speed { get; init; } 
+
+    public PointF Position { get; set; }
+    public int PathSegmentIndex { get; set; }
+    public bool IsDead => Hp <= 0;
+    public bool ReachedEnd { get; set; }
+
+    public float Radius { get; init; }
+    public int Reward { get; init; }
+}
+
+public enum EnemyType
+{
+    Grunt
+}
